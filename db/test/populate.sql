6f29ae5ffa952a7f7776b81301c4ff2522a1c287
Zoe Ames <zoeames@gmail.com>
2014-12-15 14:09:22 -0600

    travis fix for registration

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,1 +1,7 @@
+insert into users (username,password, avatar, token) values ('a', 'b', 'c', 'd');
+insert into users (username,password, avatar, token) values ('a', 'b', 'c', 'd');
+insert into users (username,password, avatar, token) values ('a', 'b', 'c', 'd');
+
+delete from users;
+
 insert into users (id,username,password, avatar, token) values (1, 'bob', '$2a$08$XBPGoYeNhBUDuHid8AWNtuimjy53zHLkwaygm1UrUHQWaPZLCzOFG', 'a.png', 'tok');
