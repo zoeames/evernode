142056c9723036822a7f7c9b2e7a1e98d455ce7a
Zoe Ames <zoeames@gmail.com>
2014-12-16 11:08:13 -0600

    all notes acceptance tests except upload and upload photo

--- test/acceptance/notes.js
+++ test/acceptance/notes.js
@@ -14,7 +14,7 @@ var expect     = require('chai').expect,
     db         = h.getdb();
 
 describe('Notes', function(){
-  var cookie;
+  var cookie, noteId;
   beforeEach(function(done){
     cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
       var options = {
@@ -24,11 +24,28 @@ describe('Notes', function(){
           username: 'bob',
           password: '123'
         }
+      },
+      options2 = {
+        method: 'post',
+        url: '/notes',
+        headers:{
+          cookie: cookie
+        },
+        payload:{
+          title: 'a',
+          body: 'b',
+          tags:'c,d,e'
+        }
       };
 
-      server.inject(options, function(response){
-        cookie = response.headers['set-cookie'][0].match(/hapi-cookie=[^;]+/)[0];
-        done();
+      server.inject(options, function(response1){
+        server.inject(options2, function(response2){
+          noteId = response2.result.noteId;
+          //console.log(response2);
+          console.log('NOTEID >>>>> ', noteId);
+          cookie = response1.headers['set-cookie'][0].match(/hapi-cookie=[^;]+/)[0];
+          done();
+        });
       });
     });
   });
@@ -70,6 +87,39 @@ describe('Notes', function(){
       });
     });
   });
+  describe('get /notes/:noteId', function(){
+    it('should get info about a note', function(done){
+      var options = {
+        method: 'get',
+        url: '/notes/'+noteId,
+        headers:{
+          cookie: cookie
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.result.title).to.equal('a');
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+  describe('delete /notes/:noteId', function(){
+    it('should delete a note', function(done){
+      var options = {
+        method: 'delete',
+        url: '/notes/'+noteId,
+        headers:{
+          cookie: cookie
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
   describe('get /notes/count', function(){
     it('should get all Notes', function(done){
       var options = {
@@ -82,7 +132,7 @@ describe('Notes', function(){
 
       server.inject(options, function(response){
         expect(response.statusCode).to.equal(200);
-        expect(response.result.count).to.equal('0');
+        expect(response.result.count).to.equal('1');
         done();
       });
     });
